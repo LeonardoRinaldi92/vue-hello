43fd5f9a32513656a00aabc3068be76dda10bb62
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-04-18 14:49:31 +0200

    completo + bonus + swap image

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,16 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        imgSrc: 'https://picsum.photos/id/237/200/300',
+        imageToSwap: 'https://picsum.photos/id/240/200/300',
       }
-    }
+    },
+    methods: {
+        cambiaImmagine(){
+            let imageToSwap = this.imgSrc
+            this.imgSrc = this.imageToSwap;
+            this.imageToSwap = imageToSwap
+        }
+    },
   }).mount('#app')
