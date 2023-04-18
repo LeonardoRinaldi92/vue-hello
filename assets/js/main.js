66b403c0517e6d22ae610f7a5d8f8f9a454269b4
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-04-18 15:12:22 +0200

    completo + swap colori e immagini

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,8 @@ const { createApp } = Vue
         message: 'Hello Vue!',
         imgSrc: 'https://picsum.photos/id/237/200/300',
         imageToSwap: 'https://picsum.photos/id/240/200/300',
+        colors: ['black','red','blue','green',],
+        colorsIndex: 0
       }
     },
     methods: {
@@ -13,6 +15,14 @@ const { createApp } = Vue
             let imageToSwap = this.imgSrc
             this.imgSrc = this.imageToSwap;
             this.imageToSwap = imageToSwap
+        },
+        cambiaColore(){
+            if (this.colorsIndex == this.colors.length - 1) {
+            this.colorsIndex = 0
+        }else {
+            this.colorsIndex ++
         }
+        }
+
     },
   }).mount('#app')
